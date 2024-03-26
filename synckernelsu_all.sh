3a3ca2bef649cfc359fe03f832f6f10c349193cf
Coconut <coconutat@protonmail.com>
2024-03-26 11:31:06 +0800

    build_scripts:New version for sync kernelsu

--- synckernelsu_all.sh
+++ synckernelsu_all.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-if [ -d KernelSU ];
-then
-    echo "Find KernelSU Floder,will remove it"
-	rm -rf KernelSU
+
+if [ -d KernelSU ]; then
+    echo "Found KernelSU Folder, removing it..."
+    rm -rf KernelSU
 	rm -rf drivers/kernelsu
 else
-	echo "Not find KernelSU Floder,good."
+    echo "KernelSU Folder not found, proceeding..."
 fi
 
+read -p "Please enter the version number (A/a is tags version.B/b is main version.): " version
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+if [ "$version" == "" ]; then
+    echo "No version specified. Exiting."
+    exit 1
+fi
+
+case $version in
+    [Aa]*)
+        read -p "Please enter the version number you want to install: " specified_version
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s "$specified_version"
+        ;;
+    [Bb]*)
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+        ;;
+    *)
+        echo "Invalid option. Exiting."
+        exit 1
+        ;;
+esac
