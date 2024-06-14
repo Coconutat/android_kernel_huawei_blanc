0bd99742e0d2dd4d45e0839bf1e9072ba1933d60
Coconut <coconutat@protonmail.com>
2024-06-14 14:03:59 +0800

    build_script : change build target

--- build_kernel.sh
+++ build_kernel.sh
@@ -24,7 +24,7 @@ start_time=$(date +%Y.%m.%d-%I_%M)
 
 start_time_sum=$(date +%s)
 
-make ARCH=arm64 O=out CC="ccache clang -fcolor-diagnostics -fdiagnostics-color=always" merge_kirin970_ksu_defconfig
+make ARCH=arm64 O=out CC="ccache clang -fcolor-diagnostics -fdiagnostics-color=always" merge_kirin970_mod_defconfig
 # 定义编译线程数
 make ARCH=arm64 O=out CC="ccache clang -fcolor-diagnostics -fdiagnostics-color=always" -j$(nproc --all) 2>&1 | tee kernel_log-${start_time}.txt
 
