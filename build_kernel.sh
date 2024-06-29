5edf416c1ef3df993bd23aa325ed73ae66a76418
Coconut <coconutat@protonmail.com>
2024-06-29 19:54:05 +0800

    defconfig: Disable CONFIG_HISI_DUMMY_KO

--- build_kernel.sh
+++ build_kernel.sh
@@ -57,9 +57,13 @@ if [ -f out/arch/arm64/boot/Image.gz ]; then
 	./tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x37E00000 --kernel_offset 0x00080000 --ramdisk_offset 0x37600000 --header_version 1 --os_version 10 --os_patch_level 2020-11-01 --output Mate10Pro_kernel.img
 	./tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x37E00000 --kernel_offset 0x00080000 --ramdisk_offset 0x37600000 --header_version 1 --os_version 10 --os_patch_level 2020-11-01  --output Mate10Pro_PM_kernel.img
 	echo " "
+	git reset --hard
+	rm -rf KernelSU
 	exit 0
 else
 	echo " "
+	git reset --hard
+	rm -rf KernelSU
 	echo "***Failed!***"
 	exit 0
 fi
